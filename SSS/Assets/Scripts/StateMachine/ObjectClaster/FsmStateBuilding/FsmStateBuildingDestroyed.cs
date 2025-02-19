c6a5328ce6f4861fc22769e5f9ef0fa6688ed08a
Fossa2016 <Fossa-2016@yandex.ru>
2025-02-19 04:34:03 +0300

    В добавление предыдущему коммиту: добавлена абстрактная дверь с ХП, перекрывающая проход врагам. Враги агрятся на ГГ если он подойдёт к ним близко, даже если у них сейчас другая цель для движения/драки.

--- SSS/Assets/Scripts/StateMachine/ObjectClaster/FsmStateBuilding/FsmStateBuildingDestroyed.cs
+++ SSS/Assets/Scripts/StateMachine/ObjectClaster/FsmStateBuilding/FsmStateBuildingDestroyed.cs
@@ -10,12 +10,12 @@ public class FsmStateBuildingDestroyed : FsmStateBuilding
 
     public override void Enter()
     {
-        Debug.Log("BuildingDestroyed state [ENTER]");
+        Debug.Log("Building Destroyed state [ENTER]");
         building.gameObject.SetActive(false);
     }
 
     public override void Exit()
     {
-        Debug.Log("BuildingDestroyed state [EXIT]");
+        Debug.Log("Building Destroyed state [EXIT]");
     }
 }
