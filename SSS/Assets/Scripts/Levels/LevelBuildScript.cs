a6d038b3c165e8d03a88dec4b31a1858776e3def
Fossa2016 <Fossa-2016@yandex.ru>
2025-03-27 17:12:53 +0300

    дрочь.

--- SSS/Assets/Scripts/Levels/LevelBuildScript.cs
+++ SSS/Assets/Scripts/Levels/LevelBuildScript.cs
@@ -97,7 +97,7 @@ public class LevelBuildScript : MonoBehaviour
     {
         SettingsMenu[] allObjects = Resources.FindObjectsOfTypeAll<SettingsMenu>();
         allObjects[0].Awake(); // ну и фигня, нельзя к Instance обратиться, бо он инициализируется у нас в Awake
-        SaveLoadManager.Instance.ImplementStoredSettings(); // чтоб настройки применялись при загрузке сцены сразу, а не после открытия меню настроек
+        SaveLoadManager.Instance.ImplementStoredSettings(); // чтоб настройки применялись при загрузке сцены сразу, а не после открытия меню настроек 
     }
 
     // Update is called once per frame
